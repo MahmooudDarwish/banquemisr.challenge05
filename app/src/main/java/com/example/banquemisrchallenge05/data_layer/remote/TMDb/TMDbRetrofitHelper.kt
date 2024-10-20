ef171515350974bdef3ef6b137dd49a133e6cc51
pc <mahmoudsaaddarwish1@gmail.com>
2024-10-21 00:22:27 +0300

    Add caching mechanism to retrofit

--- app/src/main/java/com/example/banquemisrchallenge05/data_layer/remote/TMDb/TMDbRetrofitHelper.kt
+++ app/src/main/java/com/example/banquemisrchallenge05/data_layer/remote/TMDb/TMDbRetrofitHelper.kt
@@ -1,16 +1,26 @@
 package com.example.e_store.utils.data_layer.remote.shopify
 
+import com.airbnb.lottie.compose.BuildConfig
 import com.example.banquemisrchallenge05.utils.constants.APIKeys
+import okhttp3.Cache
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
 
 
 object TMDbRetrofitHelper {
 
+    private fun getCacheDir(): File {
+        val cachePath = "${System.getProperty("java.io.tmpdir")}/httpCache"
+        return File(cachePath).apply { mkdirs() }
+    }
+
     val api: TMDbAPIServices by lazy {
+        val cacheSize = 10 * 1024 * 1024
+        val cache = Cache(getCacheDir(), cacheSize.toLong())
 
         val headerInterceptor = Interceptor { chain ->
             val request = chain.request().newBuilder()
@@ -21,12 +31,13 @@ object TMDbRetrofitHelper {
 
         val clientBuilder = OkHttpClient.Builder()
             .addInterceptor(headerInterceptor)
+            .cache(cache)
 
-
-        val logging = HttpLoggingInterceptor()
-        logging.setLevel(HttpLoggingInterceptor.Level.BODY)
-        clientBuilder.addInterceptor(logging)
-
+        if (BuildConfig.DEBUG) {
+            val logging = HttpLoggingInterceptor()
+            logging.setLevel(HttpLoggingInterceptor.Level.BODY)
+            clientBuilder.addInterceptor(logging)
+        }
 
         val client = clientBuilder.build()
 
@@ -38,8 +49,4 @@ object TMDbRetrofitHelper {
             .create(TMDbAPIServices::class.java)
     }
 }
-
-
-
-
 
