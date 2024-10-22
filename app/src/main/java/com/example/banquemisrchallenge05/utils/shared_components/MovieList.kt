bebed437b393f7d4293095c8c8a8cf2c3fdf6931
pc <mahmoudsaaddarwish1@gmail.com>
2024-10-23 01:51:58 +0300

    Enhance the restore of the internet

--- app/src/main/java/com/example/banquemisrchallenge05/utils/shared_components/MovieList.kt
+++ app/src/main/java/com/example/banquemisrchallenge05/utils/shared_components/MovieList.kt
@@ -1,11 +1,18 @@
 package com.example.banquemisrchallenge05.utils.shared_components
 
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavController
 import com.example.banquemisrchallenge05.R
 import com.example.banquemisrchallenge05.utils.shared_models.DataState
 import com.example.banquemisrchallenge05.utils.shared_models.Movie
+import com.example.weather.utils.managers.InternetChecker
 
 @Composable
 fun MovieList(
@@ -15,10 +22,24 @@ fun MovieList(
     currentPage: Int,
     onMovieChange: (Int) -> Unit,
 ) {
+    val context = LocalContext.current
+    val internetChecker = remember { InternetChecker(context) }
+    var isInternetAvailable by remember { mutableStateOf(false) }
+
     LaunchedEffect(Unit) {
-        onFetchMovies()
+
+        internetChecker.startMonitoring()
+        internetChecker.networkStateFlow.collect { isConnected ->
+            isInternetAvailable = isConnected
+            onFetchMovies()
+        }
     }
 
+    DisposableEffect(Unit) {
+        onDispose {
+            internetChecker.stopMonitoring()
+        }
+    }
     when (moviesUiState) {
         is DataState.Loading -> {
             MoviesLoadingIndicator()
